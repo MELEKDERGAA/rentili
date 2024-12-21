bcf482688a4ea74fbfe0c84443f783cdef2f7a98
-Arthur- <47183831+MELEKDERGAA@users.noreply.github.com>
2024-12-21 14:45:46 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+@section("title", "Home")
+@section('content')
+    <div class="container">
+        <h1>Home</h1>
+        <p>Welcome to our website</p>
+    </div>
+@endsection
